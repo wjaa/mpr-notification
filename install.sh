069c2f79177c020f3e951713ce09c04bb740b1c3
Wagner Jeronimo <wag182@gmail.com>
2019-04-11 16:17:47 -0300

    correcao do install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "INICIANDO BUILD DO NOTIFICATION"
 echo "PARANDO OS CONTAINERS"
-docker stop notification && docker stop nginx-nt
+docker stop ws && docker stop nginx && docker stop nginx-nt && docker stop notification
 
 #pull e build do mpr-notification
 git pull && mvn clean install
@@ -15,6 +15,9 @@ if [ -f "$path" ]; then
     echo "Iniciando o compose"
     docker-compose up -d
     echo "FIM DO BUILD"
+    echo "ESPERA!!!! PRECISA SUBIR OS OUTROS CONTAINERS...."
+    docker start ws && docker start nginx
+    echo "AGORA FECHOU!"
 
 else
     echo "ERROR: Algum erro no build não gerou o jar. Implementar envio de email"
